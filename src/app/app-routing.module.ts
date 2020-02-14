058f35352e6c861ccd3cf60b5e5efda03fae578c
bychkovei <bychkovei@it2g.ru>
2020-02-14 16:10:52 +0300

    App init. Material integration. Add basic components. Configure simple routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { AboutComponent } from "./components/about/about.component";
+import { MainPageComponent } from "./components/main-page/main-page.component";
+import { PostsComponent } from "./components/posts/posts.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "home", component: MainPageComponent },
+  { path: "posts", component: PostsComponent },
+  { path: "about", component: AboutComponent },
+  { path: "**", component: MainPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
+import { LayoutModule } from "@angular/cdk/layout";
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AComponent } from "./a/a.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { AboutComponent } from "./components/about/about.component";
+import { MainPageComponent } from "./components/main-page/main-page.component";
+import { PostsComponent } from "./components/posts/posts.component";
+import { MaterialModule } from "./shared/material.module";
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    AComponent,
+    PostsComponent,
+    AboutComponent
   ],
   imports: [
+    MaterialModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
